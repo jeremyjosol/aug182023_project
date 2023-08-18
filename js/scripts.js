0ca23e455ac0bbe564fb2aa3619a0d64314c403c
Jeremy Josol <jeremyjosol@gmail.com>
2023-08-18 10:55:13 -0700

    Test 11 pass, refactor tests 8, 9, 10

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,12 @@
 function mrRoboger(number){
   let numbersArray = number.split('');
   for (let i = 0; i < numbersArray.length; i++){
-    if (numbersArray[0] === "3" || (numbersArray[1] === "3")){
+    if (numbersArray.includes("3")){
       return "Won't you be my neighbor?";
-    } else if (numbersArray[0] === "2" || numbersArray[1] === "2"){
+    } else if (numbersArray.includes("2")){
       return "Boop!";
-    } else if (numbersArray[0] === "3"){
-      return "Won't you be my neighbor?";
-    } else if (numbersArray[0] === "0" || numbersArray[0] >= 4 || numbersArray[0] <= 9)
-      return numbersArray[0];
-  }}
+    } else if (numbersArray.includes("1")){
+      return "Beep!";
+    } else 
+      return numbersArray.join('');
+  }} 

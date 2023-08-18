b03596df298be23b31fb47acb2521e62a30ac911
Jeremy Josol <jeremyjosol@gmail.com>
2023-08-18 09:42:12 -0700

    Initialize test 5

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,8 @@
 function mrRoboger(number){
   let numbersArray = number.split(' ');
   for (let i = 0; i < numbersArray.length; i++){
-    if (numbersArray[i][0] === "1")
+    if (numbersArray[i][0] === "1"){
       return "Beep!";
+    } 
     } 
   }

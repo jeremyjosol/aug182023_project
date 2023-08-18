8a72ad37c6528c8b3989a7b3be53ca7618eb54b1
Jeremy Josol <jeremyjosol@gmail.com>
2023-08-18 09:44:41 -0700

    Test 5 pass

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,8 @@ function mrRoboger(number){
   for (let i = 0; i < numbersArray.length; i++){
     if (numbersArray[i][0] === "1"){
       return "Beep!";
-    } 
-    } 
+    } else if (numbersArray[i][0] === "2"){
+      return "Boop!";
+    }
+  }
   }

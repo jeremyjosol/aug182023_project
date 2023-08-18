7497db4fe0c4897f0128cc1b619f780e1b5130d6
Jeremy Josol <jeremyjosol@gmail.com>
2023-08-18 13:58:03 -0700

    Add HTML img, CSS styles

--- js/scripts.js
+++ js/scripts.js
@@ -2,27 +2,37 @@
 
 function mrRoboger(number){
   let numbersArray = number.split('');
+  let result = ''
   for (let i = 0; i < numbersArray.length; i++){
     if (numbersArray.includes("3")){
-      return "Won't you be my neighbor?";
+      result = "Won't you be my neighbor?";
     } else if (numbersArray.includes("2")){
-      return "Boop!";
+      result = "Boop!";
     } else if (numbersArray.includes("1")){
-      return "Beep!";
-    } else 
-      return numbersArray.join('');
+      result = "Beep!";
+    } else if (numbersArray[i]){
+      result = numbersArray.join(", ") + "..."
+    }  
   }
+  return result;
 } 
 
 // UI logic
 
+let robogerArray = [];
+
 function userInput(event){
   event.preventDefault();
   const robogerInput = document.getElementById("input").value;
-  const results = mrRoboger(robogerInput);
-  const output = document.getElementById("output");
-  output.innerText = results;
-}
+  if(!isNaN(robogerInput)){
+    const results = mrRoboger(robogerInput);
+    robogerArray.push(results);
+    } else { 
+      robogerArray.push("Error!");
+    }
+    const output = document.getElementById("output");
+    output.innerText = robogerArray.join(' ');
+  } 
   window.addEventListener("load", function(){
     document.getElementById("roboger").addEventListener("submit", userInput);
   });

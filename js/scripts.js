34dbc2636cbc5e7fdeb83c95253a2bc3e0bcf281
Jeremy Josol <jeremyjosol@gmail.com>
2023-08-20 23:22:15 -0700

    Clean up js

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,26 @@
 // Business logic
 
 function mrRoboger(number){
-  let numbersArray = number.split('');
-  let result = ''
-  for (let i = 0; i < numbersArray.length; i++){
-    if (numbersArray.includes("3")){
-      result = "Won't you be my neighbor?";
-    } else if (numbersArray.includes("2")){
-      result = "Boop!";
-    } else if (numbersArray.includes("1")){
-      result = "Beep!";
-    } else if (numbersArray[i]){
-      result = numbersArray.join(", ") + "..."
+  let numbersArray = [];
+  for (let i = 0; i <= parseInt(number); i++){
+    numbersArray.push(i);
+  }
+
+  let result = [];
+
+  for (let i = 0; i < numbersArray.length; i++) {
+    let robogerNumber = numbersArray[i].toString();
+    if (robogerNumber.includes("3")){
+      result.push("Won't you be my neighbor?");
+    } else if (robogerNumber.includes("2")){
+      result.push("Boop!");
+    } else if (robogerNumber.includes("1")){
+      result.push("Beep!");
+    } else {
+      result.push(robogerNumber);
     }  
   }
-  return result;
+  return result.join(", ")
 } 
 
 // UI logic
